12c4a067774f32b5ff34eafda41558830fcbeca7
Vadim Sobolev <vadsobolev@yandex.ru>
2023-07-09 14:38:10 +0300

    c#

---
+++ Test/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] mass1 = new string[4] {"hello", "2", "world", ":-)"};
+string[] mass2 = new string[mass1.Length];
+void SecondMassWithIF(string[] mass1, string[] mass2)
+{
+    int count = 0;
+    for (int i = 0; i < mass1.Length; i++)
+    {
+    if(mass1[i].Length <= 3)
+        {
+        mass2[count] = mass1[i];
+        count++;
+        }
+    }
+}
+void PrintMass(string[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondMassWithIF(mass1, mass2);
+PrintMass(mass2);
+
+
+
+
+
+
+
+
+
+
+
+
+
